f1cf85c7cdd40526fc25f75b2d475dddcd0bd2c4
Louis Jacobowitz <superbird11@gmail.com>
2017-01-13 11:45:32 -0500

    Change sort order for class and duration

--- src/edu/ldj/planner/compare/CompareClass.java
+++ src/edu/ldj/planner/compare/CompareClass.java
@@ -13,9 +13,9 @@ public class CompareClass< T extends Task > implements Comparator<T> {
 	 * Returns a lexigraphic comparison of task1 and task2's class names
 	 * @param task1 to compare
 	 * @param task2 to compare
-	 * @return >1 if task1's class name is lexigraphically greater than task2
+	 * @return >1 if task2's class name is lexigraphically greater than task1
 	 */
 	public int compare( T task1, T task2 ) {
-		return task2.getTaskClass().compareTo( task1.getTaskClass() );
+		return task1.getTaskClass().compareTo( task2.getTaskClass() );
 	}
 }
--- src/edu/ldj/planner/compare/CompareTime.java
+++ src/edu/ldj/planner/compare/CompareTime.java
@@ -13,11 +13,11 @@ public class CompareTime< T extends Task > implements Comparator<T> {
 	 * Compares the two given tasks based on their estimated completion time
 	 * @param task1 to compare
 	 * @param task2 to compare
-	 * @return >1 if task1 is shorter than task2
+	 * @return >1 if task1 is longer than task2
 	 */
 	@Override
 	public int compare( T task1, T task2 ) {
-		return (int) ( 100 * ( task2.getTimeEstimate() - task1.getTimeEstimate() ) );
+		return (int) ( 100 * ( task1.getTimeEstimate() - task2.getTimeEstimate() ) );
 	}
 
 }
